c7a3b994e85bcae026ba576f06d4269d2ba9072f
Mario Sclavo <mmsclavo@gmail.com>
2023-11-10 15:58:34 -0300

    feature: incluida todas as operações de registro

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -3,23 +3,43 @@ import styles from './Task.module.css';
 import trash from '../assets/trash.svg';
 import circleCheck from '../assets/circle-check.svg';
 import circleChecked from '../assets/circle-checked.svg';
+import { ChangeEvent, useState } from 'react';
 
-export interface Task {
+export interface TaskModel {
   id: string;
   content: string;
+  isComplete: boolean;
 }
 
 interface TaskProps {
-  task: Task;
+  task: TaskModel;
+  onDeleteTask: (task: TaskModel) => void;
+  onChangeTask: (task: TaskModel) => void;
 }
 
-export function Task({ task }: TaskProps) {
+export function Task({ task, onDeleteTask: onDeleteTask, onChangeTask }: TaskProps) {
+  const [isChecked, setIsChecked] = useState(task.isComplete);
+
+  function handleCheckboxChange(event: ChangeEvent<HTMLInputElement>) {
+    const isChecked = event.target.checked;
+    setIsChecked(isChecked);
+    onChangeTask({...task, isComplete: isChecked});
+  }
+
+  function checkboxToggle() {
+    setIsChecked(!isChecked);
+  }
+
+  function handleOnDeleteTask() {
+    onDeleteTask(task);
+  }
+
   return (
     <div className={styles.task}>
-      <img src={circleCheck} />
-      <input type='checkbox' id={task.id} />
-      <label htmlFor={task.id}>{task.content}</label>
-      <button className={styles.trashButton}>
+      <img src={isChecked ? circleChecked : circleCheck} onClick={checkboxToggle} />
+      <input type='checkbox' id={task.id} checked={isChecked} onChange={handleCheckboxChange}/>
+      <label className={isChecked ? styles.complete : ''} htmlFor={task.id}>{task.content}</label>
+      <button className={styles.trashButton} onClick={handleOnDeleteTask}>
         <img src={trash} alt='Remover tarefa' />
       </button>
     </div>
--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -1,36 +1,86 @@
-import { Task } from './Task';
+import { ChangeEvent, FormEvent, useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
+import { Task, TaskModel } from './Task';
 import styles from './Todo.module.css';
 
 import plus from '../assets/plus.svg';
-import { useState } from 'react';
+import clipboard from '../assets/clipboard.svg';
 
-const tasksList: Task[] = [
-  {
-    id: '1',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio ducimus sint fugiat
-        illo optio voluptas dicta nostrum iure aliquam aliquid libero adipisci repellendus, velit
-        est impedit, labore, eveniet ab!`,
-  },
-  {
-    id: '2',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio ducimus sint fugiat
-        illo optio voluptas dicta nostrum iure aliquam aliquid libero adipisci repellendus, velit
-        est impedit, labore, eveniet ab!`,
-  },
-  {
-    id: '3',
-    content: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio ducimus sint fugiat
-        illo optio voluptas dicta nostrum iure aliquam aliquid libero adipisci repellendus, velit
-        est impedit, labore, eveniet ab!`,
-  },
-];
+const contentTaskMock = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit odio ducimus sint fugiat
+illo optio voluptas dicta nostrum iure aliquam aliquid libero adipisci repellendus, velit
+est impedit, labore, eveniet ab!`;
 
 export function Todo() {
+  const [taskList, setTaskList] = useState<TaskModel[]>([]);
+  const [newContentTask, setNewContent] = useState('');
+  const [countTasksCompletes, setCountTasksCompletes] = useState(0);
+
+  function handleAddNewTask(event: FormEvent) {
+    event.preventDefault();
+    const newTask: TaskModel = createNewTask(newContentTask, false);
+    const newTaskList = [newTask, ...taskList];
+    updateTaskList(newTaskList);
+    setNewContent('');
+  }
+
+  function createNewTask(content: string, isComplete: boolean): TaskModel {
+    return {
+      id: uuidv4(),
+      content: content,
+      isComplete: isComplete
+    }
+  }
+
+  function handleNewContentTask(event: ChangeEvent<HTMLInputElement>) {
+    event.preventDefault();
+    setNewContent(event.target.value);
+  }
+
+  function handleDeleteTask(taskToDelete: TaskModel) {
+    const newTaskList = taskList.filter((task: TaskModel) => task.id !== taskToDelete.id);
+    updateTaskList(newTaskList);
+  }
+
+  function updateTaskList(tasks: TaskModel[]) {
+    updateCountCompletedTasks(tasks);
+    setTaskList(tasks);
+  }
+
+  function updateTaskInTaskList(updatedTask: TaskModel) {
+    const newTaskList = taskList.map((task: TaskModel) =>  {
+      if(task.id === updatedTask.id) {
+        return updatedTask;
+      }
+      return task;
+    });
+    updateTaskList(newTaskList);
+  }
+
+  function updateCountCompletedTasks(tasks: TaskModel[]) {
+    const countCompleteTasks = tasks.reduce(
+      (count: number, task: TaskModel) => {
+        if(task.isComplete) {
+          return count + 1;
+        }
+        return count;
+      }, 0
+    );
+    setCountTasksCompletes(countCompleteTasks);
+  }
+
   return (
     <div className={styles.todo}>
       <header>
-        <form className={styles.todoForm}>
-          <input type='text' name='newTodo' placeholder='Adicione uma nova tarefa' />
+        <form className={styles.todoForm} onSubmit={handleAddNewTask}>
+          <input
+            type='text'
+            name='newTodo'
+            placeholder='Adicione uma nova tarefa'
+            value={newContentTask}
+            onChange={handleNewContentTask}
+            required
+          />
           <button>
             Criar <img src={plus} />
           </button>
@@ -40,17 +90,25 @@ export function Todo() {
         <header>
           <div className={styles.createdTasks}>
             <strong>Tarefas criadas</strong>
-            <span>5</span>
+            <span>{taskList.length}</span>
           </div>
           <div className={styles.completedTasks}>
             <strong>Concluídas</strong>
-            <span>2 de 5</span>
+            <span>{countTasksCompletes} de {taskList.length}</span>
           </div>
         </header>
         <div className={styles.todoList}>
-          {tasksList.map((task) => {
-            return <Task key={task.id} task={task} />;
-          })}
+          {taskList.length > 0 ? (taskList.map((task: TaskModel) => {
+            return <Task key={task.id} task={task} onDeleteTask={handleDeleteTask} onChangeTask={updateTaskInTaskList} />;
+          })) : (
+            <div className={styles.emptyList}>
+              <img src={clipboard} />
+              <div>
+                <strong>Você ainda não tem tarefas cadastradas</strong>
+                <span>Crie tarefas e organize seus itens a fazer</span>
+              </div>
+            </div>
+          )}
         </div>
       </main>
     </div>
